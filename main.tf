2e54aa27fea83b57faddbebf3ecfc7b5bd0d6c78
satishcloudsec <95839490+satishcloudsec@users.noreply.github.com>
2023-07-20 19:08:01 +0530

    Added s3 bucket

--- main.tf
+++ main.tf
@@ -24,7 +24,12 @@ provider "aws" {
 	region="ap-south-1"
 }
 
-resource "aws_instance" "my_ec2"{
+/*resource "aws_instance" "my_ec2"{
 	ami="ami-0f5ee92e2d63afc18"
 	instance_type="t2.micro"
+}*/
+
+resource "aws_s3_bucket" "my_s3"
+{
+	bucket="tfdemobucketnull"
 }
