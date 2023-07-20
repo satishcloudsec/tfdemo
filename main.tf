1e1ec04503e0d51903f39ff70fb04fdb2cd0ca41
satishcloudsec <satish.cloudsec@gmail.com>
2023-07-20 11:43:36 +0530

    EC2 instance demo

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 # To provision EC2 instance
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66"
+    }
+  }
+}
+
+
 provider "aws" {
 	region="ap-south-1"
 }
