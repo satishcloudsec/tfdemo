8427fd3ceeb2215c7d928f797f1806fb94f0bae0
satishcloudsec <satish.cloudsec@gmail.com>
2023-07-29 11:06:35 +0530

    null meet -1

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_s3"{
 	# checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
-	# checkovtest:skiptestqwert=CKV2_AWS_6: "Ensure that S3 bucket has a Public Access block"
+	# checkov:skip=CKV2_AWS_6: "Ensure that S3 bucket has a Public Access block"
 	# checkov:skip=CKV2_AWS_61:Ensure that an S3 bucket has a lifecycle configuration
 	# checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
 	# checkov:skip=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
